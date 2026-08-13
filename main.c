535d96a2d75c94f7e77350b9f9f2febbf5998cef
rishi-vinod <rishivinod08@gmail.com>
2026-08-12 20:35:21 -0400

    feat: main

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
-int main() {
-    
+#include "gpio.h"
+
+void delay(volatile uint32_t count) {
+    while(count--) {
+        asm volatile("nop");
+    }
+}
+
+void kernel_main(void) {
+    gpio_set_output(9); /* 9 is the onboard LED on the Pi */
+
+    while(1) { /* Since there is no operating system - we need a way for the command following this to continue without the CPU crashing. */
+        gpio_set_high(9);
+        delay(300000000);
+        gpio_set_low(9);
+        delay(300000000);
+    }
 }
